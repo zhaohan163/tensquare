ba4a8dba62dd7e10f251d032cb7b8d1e08785c86
zhaohan <13853084065@163.com>
2019-01-21 16:58:26 +0800

    提交到git 完成用户注册微服务

---
+++ tensquare_sms/src/main/java/com/tensquare/sms/SmsApplication.java
@@ -0,0 +1,18 @@
+package com.tensquare.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName SmsApplication
+ * @Author 123456
+ * @Description TODO
+ * @Version 1.0
+ * @Date 2019/1/21 16:15
+ **/
+@SpringBootApplication
+public class SmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class,args);
+    }
+}
